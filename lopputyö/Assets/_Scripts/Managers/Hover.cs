5c8e9e52660aab70e4cb6614d678bf0178877288
TrueTipu <59774591+TrueTipu@users.noreply.github.com>
2023-05-03 21:28:13 +0300

    mapeditor asia aluille

---
+++ lopputyö/Assets/_Scripts/Managers/Hover.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+/// <summary>
+///Hallitsee hiirtä ja hoveria
+/// </summary>
+public class Hover : MonoBehaviour
+{
+    LevelGridManager grid;
+
+    Vector3 rememberPos;
+    Tile rememberTile;
+    Tile currentTile;
+
+    private void Start()
+    {
+        grid = LevelGridManager.Instance;
+    }
+
+    private void Update()
+    {
+        MouseCheck();
+        CheckKeyboard();
+
+        transform.position = currentTile.transform.position;
+
+    }
+
+    /// <returns> Palauttaa true/false sen mukaan vaihtuiko ruutu </returns>
+    void MouseCheck()
+    {
+        Vector3 _mousePos = Helpers.GetMousePosition();
+        if (_mousePos == rememberPos)
+        {
+            return;
+        }
+        else { rememberPos = _mousePos; }
+
+        currentTile = grid.GetTile(_mousePos);
+    }
+
+    void CheckKeyboard()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            currentTile = grid.GetTile(currentTile.TileCords.x, currentTile.TileCords.y + 1);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            currentTile = grid.GetTile(currentTile.TileCords.x, currentTile.TileCords.y - 1);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentTile = grid.GetTile(currentTile.TileCords.x + 1, currentTile.TileCords.y);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentTile = grid.GetTile(currentTile.TileCords.x - 1, currentTile.TileCords.y);
+        }
+    }
+
+}
---
+++ lopputyö/Assets/_Scripts/Managers/LevelGridManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+/// <summary>
+///Hallitsee gridiä ja sen tilejä
+/// </summary>
+public class LevelGridManager : Singleton<LevelGridManager>
+{
+    [SerializeField] int width, height;
+    [SerializeField] float tileSize;
+    [SerializeField] int gridOffset;
+
+    public Tile[,] GetAllTiles()
+    {
+        return tiles;
+    }
+
+    [SerializeField] Tile tilePrefab;
+    Tile[,] tiles;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        tiles = new Tile[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector3 pos = new Vector3(x * tileSize - gridOffset, y * tileSize - gridOffset);
+                Tile spawnedTile = Instantiate(tilePrefab, pos, Quaternion.identity);
+                spawnedTile.SetName($"Tile {x}, {y}");
+
+                bool darker = (x + y) % 2 == 1; //shakkipattern
+                spawnedTile.Init(darker, new Vector2(x, y));
+
+                tiles[x, y] = spawnedTile;
+            }
+        }
+    }
+
+
+    public Tile GetTile(Vector3 _worldPos)
+    {
+        int x = Mathf.RoundToInt((_worldPos.x + gridOffset) / tileSize);
+        int y = Mathf.RoundToInt((_worldPos.y + gridOffset) / tileSize);
+        return GetTile(x, y);
+    }
+
+    public Tile GetTile(float _x, float _y)
+    {
+        int x = Mathf.RoundToInt(_x);
+        int y = Mathf.RoundToInt(_y);
+        if (x >= width) { x = width - 1; }
+        if (y >= width) { y = height - 1; }
+        if (x <= 0) { x = 0; }
+        if (y <= 0) { y = 0; }
+
+        return tiles[x, y];
+    }
+
+
+
+}
